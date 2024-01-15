ec52e839668b146f282932eed1b87d83822c1cf2
MartinTMiranda <jtaco@belcorp.biz>
2024-01-14 21:18:25 -0500

    adding_features

--- exe_python_files.sh
+++ exe_python_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define an array of file names
-files=("/home/userland/wong2.py" "/home/userland/tailoy.py" "/home/userland/hiraoka.py" "/home/userland/tottus.py" "/home/userland/saga.py" "/home/userland/send_email_2.py")
+files=("./wong2.py" "./tailoy.py" "./hiraoka.py" "./tottus.py" "./saga.py" "./send_email_2.py")
 
 # Iterate over the array and execute each file
 for file in "${files[@]}"
